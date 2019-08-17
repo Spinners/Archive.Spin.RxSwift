258a72cf6556b0282d85ccae8e11927a9bef16c8
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-08-16 23:46:29 -0400

    Initial Commit

---
+++ Spin.RxSwift/Spin_RxSwift.h
@@ -0,0 +1,19 @@
+//
+//  Spin_RxSwift.h
+//  Spin.RxSwift
+//
+//  Created by Thibault Wittemberg on 2019-08-16.
+//  Copyright © 2019 WarpFactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Spin_RxSwift.
+FOUNDATION_EXPORT double Spin_RxSwiftVersionNumber;
+
+//! Project version string for Spin_RxSwift.
+FOUNDATION_EXPORT const unsigned char Spin_RxSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spin_RxSwift/PublicHeader.h>
+
+
